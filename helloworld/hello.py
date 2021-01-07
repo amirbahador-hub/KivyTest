0c52652a826c8e853458e2d5f861eeaff39a5ace
Amirbahador Pv <amirbahador.pv@gmail.com>
2021-01-07 12:07:57 -0500

    first kivy test

---
+++ helloworld/hello.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+
+if __name__ == "__main__":
+    my_app = TestApp()
+    my_app.run()
